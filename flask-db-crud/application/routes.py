538062e454211af318eadc5577f7b28900e6b78c
gregorule <gregorule8@gmail.com>
2022-06-15 09:29:50 +0000

    first commit

---
+++ flask-db-crud/application/routes.py
@@ -0,0 +1,37 @@
+from application import app, db
+from application.models import Games
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>"+ game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('/delete/<name>')
+def delete(name):
+    name = Games.query.first()
+    db.session.delete(name)
+    db.session.commit()
+    return "Item has been deleted"
+
+@app.route('/total')
+def total():
+    total_games = Games.query.count()
+    return str(total_games)
+    
